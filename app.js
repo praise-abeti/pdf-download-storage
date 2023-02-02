52417276c6ba8379edbfa8795ef1a84a1794591d
SoloDev1 <100441084+SoloDev1@users.noreply.github.com>
2023-02-02 19:04:33 +0000

    Added Cookie Function

--- app.js
+++ app.js
@@ -6,9 +6,43 @@ const pdfDownload = document.getElementById('pdf_download');
 const countDisplay = document.getElementById('count_display');
 
 let countDownloads = 0;
-countDisplay.innerHTML = countDownloads;
-
+let daysToLive = 365 * 10
 pdfDownload.addEventListener('click', () => {
     console.log(countDownloads++);
-    countDisplay.innerHTML = countDownloads;
+    createCookies('downloads', countDownloads, daysToLive)
+    countDisplay.innerHTML = getCookies('downloads');
+    
 });
+
+// Storage Function
+countDownloads = getCookies('downloads')
+getCookies('downloads')
+countDisplay.innerHTML = countDownloads
+
+// Delete Cookie usign 👉 deleteCookies('downloads')
+
+// Cookie Storage
+function createCookies(name, value, expires){
+    let date = new Date;
+    date.setTime(date.getTime() + (expires*24*60*60*1000))
+    let expire = 'expires=' + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expire}; path=/`
+}
+
+function deleteCookies(name){
+    createCookies(name, null, null)
+}
+
+function getCookies(name){
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArray = cDecoded.split("; ");
+    let result = null;
+
+    cArray.forEach(element =>{
+        if(element.indexOf(name) == 0){
+            result = element.substring(name.length + 1)
+        }
+    })
+
+    return result
+}
